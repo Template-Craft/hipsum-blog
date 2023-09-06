fcd460aaad2a8d2322e390fbd5126225a7ddfff4
Ko2doo <ko2doodev@gmail>
2023-09-06 19:24:33 +0500

    UPD:06.09.23 Подготовка к подключению github pages

---
+++ js/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+Fancybox.bind(document.getElementById('fancybox-gallery'), '[data-fancybox]', {
+  animated: true,
+});
+
+// wow.js
+new WOW().init();
--- src/js/main.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Fancybox.bind(document.getElementById('fancybox-gallery'), '[data-fancybox]', {
-  animated: true,
-});
-
-// wow.js
-new WOW().init();
